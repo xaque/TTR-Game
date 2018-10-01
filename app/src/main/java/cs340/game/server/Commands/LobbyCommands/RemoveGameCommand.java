46cca0465e824c697dbfe82fc0419ee5933a2f17
stsummers95 <stephenleesummers@gmail.com>
2018-10-01 02:09:30 -0600

    Added server DB and Commands

---
+++ app/src/main/java/cs340/game/server/Commands/LobbyCommands/RemoveGameCommand.java
@@ -0,0 +1,16 @@
+package cs340.game.server.Commands.LobbyCommands;
+
+import cs340.game.server.Commands.iCommand;
+import cs340.game.shared.Results;
+import cs340.game.shared.data.Data;
+
+/**
+ * Created by Stephen on 10/1/2018.
+ */
+
+public class RemoveGameCommand implements iCommand {
+    public Results execute(Data data) {
+        //TODO implement operation for lobby to delete full games
+        return null;
+    }
+}
