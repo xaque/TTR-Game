719801c26cbafb39ea58aba305ef524450e6f3da
xaque <zacka999@gmail.com>
2018-10-26 04:02:34 -0600

    Created class GameStateDiff

---
+++ shared/src/main/java/cs340/game/shared/models/GameStateDiff.java
@@ -0,0 +1,49 @@
+package cs340.game.shared.models;
+
+import java.util.List;
+
+/*
+For now, perhaps we don't really do a diff, but give the whole state of the game.
+Then we can see if there is a bandwidth issue. Then we can implement it as a diff if necessary.
+ */
+public class GameStateDiff {
+    private List<Player> playerList;
+    private List<DestinationCard> destinationCardDeck;
+    private List<Route> routeList;
+    private List<TrainCard> trainCardDeck;
+
+    public GameStateDiff(){}
+
+    public List<Player> getPlayerList() {
+        return playerList;
+    }
+
+    public void setPlayerList(List<Player> playerList) {
+        this.playerList = playerList;
+    }
+
+    public List<DestinationCard> getDestinationCardDeck() {
+        return destinationCardDeck;
+    }
+
+    public void setDestinationCardDeck(List<DestinationCard> destinationCardDeck) {
+        this.destinationCardDeck = destinationCardDeck;
+    }
+
+    public List<Route> getRouteList() {
+        return routeList;
+    }
+
+    public void setRouteList(List<Route> routeList) {
+        this.routeList = routeList;
+    }
+
+    public List<TrainCard> getTrainCardDeck() {
+        return trainCardDeck;
+    }
+
+    public void setTrainCardDeck(List<TrainCard> trainCardDeck) {
+        this.trainCardDeck = trainCardDeck;
+    }
+
+}
