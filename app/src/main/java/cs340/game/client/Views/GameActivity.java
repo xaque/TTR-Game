c3e1a33ed3c3629b39b5f2ffb5ec49848f06b2f3
tapsley <tyler.apsley@gmail.com>
2018-10-30 11:55:46 -0600

    pressing the demo button now draws a route

--- app/src/main/java/cs340/game/client/Views/GameActivity.java
+++ app/src/main/java/cs340/game/client/Views/GameActivity.java
@@ -2,11 +2,19 @@ package cs340.game.client.Views;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
@@ -14,14 +22,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.support.design.widget.TabLayout;
 
+import java.util.List;
 import java.util.Objects;
 
 import cs340.game.R;
 import cs340.game.client.Presenters.GamePresenter;
+import cs340.game.shared.models.Player;
 
 
 /**
@@ -38,6 +49,22 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private Button drawDestinationsButton;
     private Button placeTrainsButton;
     private ConstraintLayout playerInfoButton;
+
+    private Canvas canvas;
+    private Paint paint = new Paint();
+    private ImageView map;
+    private Bitmap mutableBitmap;
+
+    //PAINTS
+    private Paint red = new Paint();
+    private Paint blue = new Paint();
+    private Paint green = new Paint();
+    private Paint yellow = new Paint();
+    private Paint black = new Paint();
+
+    //TEMP ROUTE
+    private int RADIUS = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,7 +119,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         playerInfoButton = findViewById(R.id.turn_order);
         playerInfoButton.setOnClickListener(this);
 
-
+        FloatingActionButton demoButton = findViewById(R.id.demoButton);
+        demoButton.setOnClickListener(this);
 
         presenter = new GamePresenter(this);
 
@@ -101,10 +129,46 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         points = (TextView) findViewById(R.id.score);
         trainsLeft = findViewById(R.id.trains_left);
+        map = findViewById(R.id.map);
+
+        //Initialize the canvas stuff
+        setPaints();
+        initializeCanvas();
 
         Toast.makeText(this, "The game has started. Good luck", Toast.LENGTH_SHORT).show();
     }
 
+    public void initializeCanvas() {
+        BitmapFactory.Options myOptions = new BitmapFactory.Options();
+        myOptions.inScaled = false;
+        myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.map ,myOptions);
+        Bitmap workingBitmap = Bitmap.createBitmap(bitmap);
+        mutableBitmap = workingBitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        ImageView imageView = (ImageView)findViewById(R.id.map);
+        imageView.setAdjustViewBounds(true);
+        imageView.setImageBitmap(mutableBitmap);
+    }
+
+    public void setPaints(){
+        red.setColor(ResourcesCompat.getColor(
+                getResources(), R.color.red, null)
+        );
+        blue.setColor(ResourcesCompat.getColor(
+                getResources(), R.color.blue, null)
+        );
+        green.setColor(ResourcesCompat.getColor(
+                getResources(), R.color.green, null)
+        );
+        yellow.setColor(ResourcesCompat.getColor(
+                getResources(), R.color.yellow, null)
+        );
+        black.setColor(ResourcesCompat.getColor(
+                getResources(), R.color.black, null)
+        );
+    }
     //************UPDATE UI FUNCTIONS**********************
 
     public void updatePoints(int points){
@@ -150,9 +214,57 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 PlayerInfoFragment playerInfoFragment = new PlayerInfoFragment();
                 playerInfoFragment.show(getSupportFragmentManager(), "player info");
                 break;
+            case R.id.demoButton:
+                presenter.runDemo();
+                break;
         }
     }
+
+
+    /* *********************UPDATE UI************************ */
+    public void setPlayerName(String name){
+        TextView playerName = findViewById(R.id.playerName);
+        playerName.setText(name);
+    }
+
+    public void setPlayers(List<Player> players){
+
+    }
+
+    public Paint getPaint(String color) {
+        switch(color) {
+            case "red":
+                return red;
+            case "blue":
+                return blue;
+            case "green":
+                return green;
+            case "yellow":
+                return yellow;
+            case "black":
+                return black;
+            default:
+                return red;
+        }
+    }
+
+    public void placeRoute(String color, int[] coords) {
+
+        Canvas canvas = new Canvas(mutableBitmap);
+
+        //So we will just have to get the proper color
+        Paint paint = getPaint(color);
+
+        //And then loop through the coordinate pairs and draw them to the screen
+        int size = coords.length;
+        for(int i = 0; i < size; i= i+2){
+            canvas.drawCircle(coords[i], coords[i+1], RADIUS, paint);
+        }
+    }
+
 }
+
+
 
 
 
@@ -177,33 +289,6 @@ class LeftPagerAdapter extends FragmentStatePagerAdapter {
                 return new CardsFragment();
             case 1:
                 return new DestinationsFragment();
-            default:
-                return null;
-        }
-    }
-
-    @Override
-    public int getCount() {
-        return mNumOfTabs;
-    }
-}
-
-class RightPagerAdapter extends FragmentStatePagerAdapter {
-    private int mNumOfTabs;
-
-    RightPagerAdapter(FragmentManager fm, int NumOfTabs){
-        super(fm);
-        this.mNumOfTabs = NumOfTabs;
-    }
-
-    @Override
-    public Fragment getItem(int position) {
-
-        switch(position) {
-            case 0:
-                return new ChatFragment();
-            case 1:
-                return new HistoryFragment();
             default:
                 return null;
         }
